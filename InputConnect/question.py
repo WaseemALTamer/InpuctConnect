e5fad24ab0c6dd1c83426e4240454faf232a5554
Waseem AL Tamer <107763849+WaseemALTamer@users.noreply.github.com>
2023-06-21 09:00:21 +1300

    Add files via upload

--- InputConnect/question.py
+++ InputConnect/question.py
@@ -29,6 +29,10 @@ def main(number):
     question_5 = ImageTk.PhotoImage(Image.open("images/question_5.png").resize((int(800),int(600))))
     question_6 = ImageTk.PhotoImage(Image.open("images/question_6.png").resize((int(800),int(600))))
     question_7 = ImageTk.PhotoImage(Image.open("images/question_7.png").resize((int(800),int(600))))
+    question_8 = ImageTk.PhotoImage(Image.open("images/question_8.png").resize((int(800),int(600))))
+    question_9 = ImageTk.PhotoImage(Image.open("images/question_9.png").resize((int(800),int(600))))
+    question_10 = ImageTk.PhotoImage(Image.open("images/question_10.png").resize((int(800),int(600))))
+    question_11 = ImageTk.PhotoImage(Image.open("images/question_11.png").resize((int(800),int(600))))
     image_question_1 = tk.Label(window, image=question_1, highlightthickness=0, bd=0)
     image_question_2 = tk.Label(window, image=question_2, highlightthickness=0, bd=0)
     image_question_3 = tk.Label(window, image=question_3, highlightthickness=0, bd=0)
@@ -36,6 +40,10 @@ def main(number):
     image_question_5 = tk.Label(window, image=question_5, highlightthickness=0, bd=0,)
     image_question_6 = tk.Label(window, image=question_6, highlightthickness=0, bd=0,)
     image_question_7 = tk.Label(window, image=question_7, highlightthickness=0, bd=0,)
+    image_question_8 = tk.Label(window, image=question_8, highlightthickness=0, bd=0,)
+    image_question_9 = tk.Label(window, image=question_9, highlightthickness=0, bd=0,)
+    image_question_10 = tk.Label(window, image=question_10, highlightthickness=0, bd=0,)
+    image_question_11 = tk.Label(window, image=question_11, highlightthickness=0, bd=0,)
     ##
     if number == 11:
         image_question_1.place(x=0,y=0)
@@ -51,5 +59,12 @@ def main(number):
         image_question_6.place(x=0,y=0)
     if number == 10:
         image_question_7.place(x=0,y=0)
-    
+    if number == 1:
+        image_question_11.place(x=0,y=0)
+    if number == 2:
+        image_question_8.place(x=0,y=0)
+    if number == 3:
+        image_question_9.place(x=0,y=0)
+    if number == 4:
+        image_question_10.place(x=0,y=0)
     window.mainloop()
